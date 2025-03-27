67a2d2c03c85a62f02a1068209ae06ef590f500b
Chokkesh0703 <chokkeshr07@gmail.com>
2025-03-27 12:12:48 +0530

    change-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,12 +10,12 @@ const Footer = () => {
   return (
     <div id="Footer">
       <div className="flex   lg:flex-row flex-col  lg:gap-0 pt-5 w-full">
-        <div className="flex flex-col  lg:px-30  gap-10  w-full ">
+        <div className="flex flex-col   gap-10  w-full p-20 pt-0   ">
           <div className="flex flex-col w-[218px] h-[118px]  bg-contain bg-no-repeat  lg:m-0">
             <img src={logo} alt="logo" className="w-full h-full" />
           </div>
-          <div className="">
-            <p className="text-[#FFFFFF] lg:text-[25px] font-Popins font-light  ">
+          <div className="lg:pr-20 ">
+            <p className="text-[#FFFFFF] lg:text-[20px] font-Popins font-light   ">
               "Ready to bring your creative vision to life? Our team at The
               Unicorn Pictures is eager to hear from you. Whether you're seeking
               stunning photography, captivating animation, or have a unique
@@ -23,19 +23,19 @@ const Footer = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-col text-[#FFFFFF] font-[Rubik] text-[10px] md:text-[20px] lg:text-[30px] justify-center   gap-10 overflow-hidden  w-full">
-          <div className="flex flex-row md:gap-45  gap-15 ">
+        <div className="flex flex-col text-[#FFFFFF] font-Rubik font-extralight text-[10px] md:text-[20px] lg:text-[20px] gap-20 overflow-hidden  w-full lg:pr-20 " >
+          <div className="flex flex-row md:gap-25   gap-15  ">
             <p>Ph no:</p>
             <p>+91 9620425828</p>
           </div>
-          <div className="flex flex-row md:gap-35 gap-10 ">
+          <div className="flex flex-row md:gap-35 lg:gap-20 gap-10 ">
             <p>Address:</p>
-            <p>
+            <p className="">
               2nd floor, 71, 15th Cross Rd, Dollar Layout, 3rd Phase, J. P.
               Nagar, Bengaluru, Karnataka 560078
             </p>
           </div>
-          <div className="flex flex-col lg:flex-row lg:gap-30 gap-10   ">
+          <div className="flex flex-col lg:flex-row lg:gap-15 gap-10   ">
             <p>Follow Us:</p>
             <div className="flex flex-row md:gap-10 gap-4 ">
               <img src={facebook} alt="facebook" className="size-10"/>
@@ -46,7 +46,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="text-[#ffff] text-[20px] flex justify-center items-center mt-20 pb-10">
+      <div className="text-[#ffff] text-[20px] flex justify-center items-center  pb-10">
         <span>&copy;</span>
         <p>Copyright 2025, All Rights Reserved</p>
       </div>

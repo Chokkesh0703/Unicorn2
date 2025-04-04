62be459526f0f6f45f1cc2ce67fea04f46598eca
chokkesh <chokkeshr07@gmail.com>
2025-04-04 16:04:39 +0530

    Update Movie.jsx

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import clipboard from './../assets/sidepic/clipboard.png'
 
 // Import images
-import image1 from './../assets/movies/97823fec3aad7753f2707638669bdaaa.png';
-import image2 from './../assets/movies/aa4a88111384767.6000a26aa0e9a.png';
-import image3 from './../assets/movies/95669260262159.5a44835b6786c.png';
-import image4 from './../assets/movies/273e787ba809e725948334340c658e68_original.png';
-import image5 from './../assets/movies/0f03e6733b0cf567cc92e8e20290462f.png';
-import image6 from './../assets/movies/Befikrre.png';
-import image7 from './../assets/movies/d9619840402943.577deee83b4c1.png';
-import image8 from './../assets/movies/Dhangal.png';
-import image9 from './../assets/movies/MV5BNTQ2ODRhNzUtMDY5ZS00NDdjLWExY2ItNDVkNGRlNTYyNDc0XkEyXkFqcGc@._V1_.png';
-import image10 from './../assets/movies/plane.png';
-import image11 from './../assets/movies/salar.png';
-import image12 from './../assets/movies/Thor.png';
+import image1 from './../assets/movies/Avengers.png';
+import image2 from './../assets/movies/Baghera.png';
+import image3 from './../assets/movies/Bahubali.png';
+import image4 from './../assets/movies/Befikrre.png';
+import image5 from './../assets/movies/Cocktail.png';
+import image6 from './../assets/movies/Dhangal.png';
+import image7 from './../assets/movies/plane.png';
+import image8 from './../assets/movies/Sahoo.png';
+import image9 from './../assets/movies/salar.png';
+import image10 from './../assets/movies/Sultan.png';
+import image11 from './../assets/movies/Thor.png';
+import image12 from './../assets/movies/Tiger.png';
 import image13 from './../assets/movies/UI.png';
 import image14 from './../assets/movies/unnamed.png';
 
@@ -31,17 +31,17 @@ function Movie() {
         
     <div className="flex gap-3 overflow-x-scroll  no-scrollbar">
       {images.map((image, index) => (
-          <div key={index} className="flex-none w-[300px] h-[700px] group hover:w-[500px] ">
+          <div key={index} className="flex-none w-[300px] h-[700px] group ">
           <img
             src={image}
             alt={`Movie ${index + 1}`}
-            className="w-full h-full   group-hover:transition-normal "
+            className="w-full h-full   "
             />
         </div>
       ))}
     </div>
     </div>
-    <div className="flex flex-col lg:flex-row gap-5 lg:gap-0 justify-center items-center  font-[Poppins] " >
+    {/* <div className="flex flex-col lg:flex-row gap-5 lg:gap-0 justify-center items-center  font-[Poppins] m-0 " >
         <div className="w-[100px] h-[80px] md:w-50 md:h-40 lg:w-[400px] lg:h-[300px]  lg:m-[40px] ml-0 md:p-10 md:px-2 "
         style={{
           backgroundImage:`url(${clipboard})`,
@@ -50,7 +50,7 @@ function Movie() {
           opacity:'100%',
         }}></div>
         <p className='text-[#FFFFFF] md:text-[18px] text-[10px] w-[80%] text-center'>"We're driving creative excellence in animation and photography, leading with innovation and technology."</p>
-    </div>
+    </div> */}
     </div>
     </>
   );
